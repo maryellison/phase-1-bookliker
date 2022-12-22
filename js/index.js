5d5b1cf132f1431b013ca7a093231c7b8946dfa8
Mary Ellison Barlow <maryellisonbarlow@gmail.com>
2022-12-22 10:10:08 -0500

    commit tree f910c0756b9fc9e789a6c57f63a48bb6fbc58532

--- js/index.js
+++ js/index.js
@@ -1,1 +1,41 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", function() {
+    getBooks();
+});
+
+
+function getBooks () {
+    fetch(`http://localhost:3000/books`)
+    .then(resp => resp.json())
+    .then(resp => resp.forEach(book => addBookToDom(book)))
+
+}
+
+function addBookToDom(book){
+    const listUL = document.getElementById('list')
+    const li = document.createElement('li')
+    li.id = book.id
+    li.textContent = book.title
+    li.addEventListener("click", (e) => showBookDetails(e, book))
+    listUL.append(li)
+}
+
+function showBookDetails(e, book) {
+    e.preventDefault();
+    const showPanel = document.querySelector('div#show-panel')
+    showPanel.innerHTML = ''
+    const image = document.createElement('img')
+    image.src = book.img_url
+    const titleH1 = document.createElement('h1')
+    titleH1.textContent = book.title
+    const descriptionP = document.createElement('p')
+    descriptionP.textContent = book.description
+    const userUL = document.createElement('ul')
+    book.users.forEach(user => {
+        const userLI = document.createElement('li')
+        userLI.textContent = user.username
+        userUL.append(userLI)
+    })
+
+    showPanel.append(image, titleH1, descriptionP, userUL)
+
+}
